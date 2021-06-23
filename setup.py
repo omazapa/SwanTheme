cd9d692b6c00bd1ceeaba19e1a78267f68e8d74c
Omar Zapata <Omar.Zapata@cern.ch>
2021-06-24 00:15:07 +0200

    fixed package creation

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@ swantheme setup
 """
 import json
 from pathlib import Path
-
+from jupyter_packaging import (
+    create_cmdclass,
+    install_npm,
+    ensure_targets,
+    combine_commands,
+    skip_if_exists,
+    get_data_files
+)
 import setuptools
 
 HERE = Path(__file__).parent.resolve()
@@ -19,6 +26,8 @@ ensured_targets = [
     str(lab_path / "static/style.js")
 ]
 
+package_data_spec = {name: ["*"]}
+
 labext_name = "@swan/theme"
 
 data_files_spec = [
@@ -27,6 +36,20 @@ data_files_spec = [
 ]
 
 long_description = (HERE / "README.md").read_text()
+cmdclass = create_cmdclass(
+    "jsdeps", package_data_spec=package_data_spec, data_files_spec=data_files_spec
+)
+
+js_command = combine_commands(
+    install_npm(HERE, build_cmd="build:prod", npm=["jlpm"]),
+    ensure_targets(ensured_targets),
+)
+
+is_repo = (HERE / ".git").exists()
+if is_repo:
+    cmdclass["jsdeps"] = js_command
+else:
+    cmdclass["jsdeps"] = skip_if_exists(jstargets, js_command)
 
 # Get the package info from package.json
 pkg_json = json.loads((HERE / "package.json").read_bytes())
@@ -41,6 +64,8 @@ setup_args = dict(
     license=pkg_json["license"],
     long_description=long_description,
     long_description_content_type="text/markdown",
+    cmdclass=cmdclass,
+    data_files=get_data_files(data_files_spec), 
     packages=setuptools.find_packages(),
     install_requires=[
         "jupyter_server>=1.6,<2"
@@ -51,7 +76,7 @@ setup_args = dict(
     platforms="Linux, Mac OS X, Windows",
     keywords=["Jupyter", "JupyterLab", "JupyterLab3"],
     classifiers=[
-        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
@@ -62,19 +87,6 @@ setup_args = dict(
     ],
 )
 
-try:
-    from jupyter_packaging import (
-        wrap_installers,
-        npm_builder,
-        get_data_files
-    )
-    post_develop = npm_builder(
-        build_cmd="install:extension", source_dir="src", build_dir=lab_path
-    )
-    setup_args['cmdclass'] = wrap_installers(post_develop=post_develop, ensured_targets=ensured_targets)
-    setup_args['data_files'] = get_data_files(data_files_spec)
-except ImportError as e:
-    pass
 
 if __name__ == "__main__":
     setuptools.setup(**setup_args)
